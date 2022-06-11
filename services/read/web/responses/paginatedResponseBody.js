190b1c2359ee9296f315086a58a2be0db4000714
Oglop <stefgust@gmail.com>
2022-06-11 09:27:46 +0200

    try result pagination

---
+++ services/read/web/responses/paginatedResponseBody.js
@@ -0,0 +1,12 @@
+module.exports = {
+    _links: {
+        next: '',
+        self:''
+    },
+    limit: 0,
+    size: 0,
+    start: 0,
+    result: [
+
+    ]
+}
--- services/read/web/routes/getQueueById.js
+++ services/read/web/routes/getQueueById.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const { requestLogger } = require('../middleware/requestLogger')
+const { validateAccessToken } = require('../middleware/validateAccessToken')
+const paginatedResponseBody = require('../responses')
+const { copyObject } = require('../../../../lib')
 /**
  * [] should have id
  */
-module.exports = router.get('/:id', requestLogger,  (req, res, next) => {
-    //insert into datastore
-    res.status(200).json({
-        offset:0,
-        limit:10,
-        data: {
-            hello: 'world'
-        }
-    });
+module.exports = router.get('/:id', requestLogger, validateAccessToken, (req, res, next) => {
+
+    const data = copyObject(paginatedResponseBody)
+    data.start = parseInt(req.query.start) || 0
+    data.limit = parseInt(req.query.limit) || 0
+    data.result.push({
+        hello: 'world'
+    })
+    
+    res.status(200).json(data);
 });
