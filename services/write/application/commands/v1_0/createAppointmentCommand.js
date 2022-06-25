ebaff5e614ff8e8bd48a6880797aa313601007d9
Oglop <stefgust@gmail.com>
2022-06-25 15:05:23 +0200

    reworking write

--- services/write/application/commands/v1_0/createAppointmentCommand.js
+++ services/write/application/commands/v1_0/createAppointmentCommand.js
@@ -1,8 +1,10 @@
 const { createAppointment } = require('../../../infrastructure/inserts/v1_0/createAppointment')
 const { debug, error } = require('../../../../../logger')
+const { generateId } = require('../../../../../lib')
 const execute = async (appointment, id) => {
     debug(`create appointment id: ${id}.`, __filename, 'execute', 'pre create')
     try {
+        const id = generateId()
         appointment.id = id
         const result = await createAppointment(appointment)
         debug(`create appointment id: ${id}.`, __filename, 'execute', 'post created')
--- services/write/web/routes/v1_0/appointmentRoutes.js
+++ services/write/web/routes/v1_0/appointmentRoutes.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const router = express.Router();
-const { copyObject } = require('../../../../../lib')
+const { copyObject, generateId } = require('../../../../../lib')
 const commandSuccessfulBody = require('../../responses/commandSuccessfulBody')
 const { validateAccessToken } = require('../../middleware/validateAccessToken')
 const { jsonSchemaRequestValidation } = require('../../middleware/jsonSchemaValidation')
-const { generateId } = require('../../../../../lib')
+const { error } = require('../../../../../logger')
 const { appointmentSchema } = require('../../schemas').v1_0
 const { executeCommand } = require('../../../application/commands/commandsHandler')
 const { CREATE_APPOINTMENT } = require('../../../common/enums').COMMANDS
-const EventEmitter = require('events')
-const eventEmitter = new EventEmitter()
+// const createAppointmentCommand = require('../../../application/commands/v1_0/createAppointmentCommand')
+
+// const EventEmitter = require('events')
+// const eventEmitter = new EventEmitter()
 
 
 eventEmitter.on(CREATE_APPOINTMENT, async (data, id) => {
@@ -21,11 +23,21 @@ eventEmitter.on(CREATE_APPOINTMENT, async (data, id) => {
 })
 
 module.exports = router.post('/', jsonSchemaRequestValidation(appointmentSchema),  validateAccessToken, async (req, res, next) => {
-    const id = generateId()
-    eventEmitter.emit(CREATE_APPOINTMENT, req.body, id)
-    const success = copyObject(commandSuccessfulBody)
-    success.id = id
-    success.message = 'created'
-    success.time = new Date().toISOString()
-    res.status(201).send(success)
+    try {
+        // const result = await createAppointmentCommand.execute(req.body)
+        const id = generateId()
+        const result = await executeCommand({
+            command: CREATE_APPOINTMENT, 
+            data: req.body, 
+            id
+        })
+        const success = copyObject(commandSuccessfulBody)
+        success.id = id
+        success.message = 'created'
+        success.time = new Date().toISOString()
+        res.status(201).send(success)
+    } catch (e) {
+        error(`message: ${e.message}`, __filename, 'post/', 'create')
+        res.status(500).end()
+    }
 });
